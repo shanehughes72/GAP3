20a783cca97716953f8fac66804d8c3b9eb5ece8
Shane Hughes <shughes@student.yosemite.edu>
2015-05-28 15:45:44 -0700

    Initial Commit

---
+++ GAP3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GAP3
+//
+//  Created by Worship Arts on 5/28/15.
+//  Copyright (c) 2015 Shane Hughes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
